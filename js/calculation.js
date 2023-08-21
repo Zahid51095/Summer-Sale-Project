881a246729e0385117046e0af4fcdc3b5b65742b
Zahid Hasan <zahidhasan140338@gmail.com>
2023-08-21 15:26:39 +0600

    added the bonus part

--- js/calculation.js
+++ js/calculation.js
@@ -7,8 +7,6 @@ const applyButton = document.getElementById("apply");
 applyButton.disabled = true;
 
 function handleClickBtn(target) {
-    // const couponInput = document.getElementById("apply");
-    // const couponCode = couponInput.value;
     const selectedItemContainer = document.getElementById("selected-items");
     const count = selectedItemContainer.childElementCount;
     const itemName = target.parentNode.childNodes[3].innerText;
@@ -18,32 +16,75 @@ function handleClickBtn(target) {
 
     const price = target.parentNode.childNodes[5].innerText.split(" ")[0];
     total = parseInt (total) + parseInt (price);
-    document.getElementById("total").innerText = total;
+    document.getElementById("total").innerText = total.toFixed(2);
     
    if(total>0) {
     purchaseButton.disabled = false;
    }
 
-    let discount = 0;
-    let newTotal = 0
+   
     if(total >= 200 ) {
         applyButton.disabled = false;
-        discount = (total * 20)/100;
-        newTotal = total - discount;
+       
 
     }
     else {
-        discount = 0;
-        newTotal = total;
+        
         
     }
     
-    document.getElementById("discount").innerText = discount;
-    document.getElementById("dis-total").innerText = newTotal;
+  
+
+
   
 }
 
 
 
 
+document.getElementById('apply').addEventListener('click', function(){
+    const applyInputField = document.getElementById('coupon-input');
+    const inputValueString = applyInputField.value;
+    applyInputField.value = '';
+   
 
+    const mainTotalElement = document.getElementById('total');
+    const mainTotalElementString = mainTotalElement.innerText;
+    const mainTotal = parseFloat(mainTotalElementString);
+
+   
+
+  
+    let discount = 0;
+    let newTotal = 0;
+
+    if(inputValueString === 'SELL200'){
+        
+        discount = (mainTotal * 20)/100;
+        newTotal = mainTotal - discount;
+
+    } else {
+        discount = 0;
+        newTotal = mainTotal;
+    }
+
+    document.getElementById('discount').innerText = discount.toFixed(2);
+    document.getElementById('dis-total').innerText = newTotal.toFixed(2);
+
+
+})
+
+function resetAndGoHome(){
+    const selectedItemsContainer = document.getElementById('selected-items');
+    selectedItemsContainer.innerHTML = '';
+
+    const totalElement = document.getElementById('total');
+    const discountElement = document.getElementById('discount');
+    const discountedTotal = document.getElementById('dis-total');
+
+    totalElement.innerText = "00";
+    discountElement.innerText = "00";
+    discountedTotal.innerText = "00";
+
+    location.reload();
+}
